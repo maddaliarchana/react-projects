bab93b8d6aa135d15e82f27a563eace9bff262d1
maddaliarchana <archanasreekanth25@gmail.com>
2025-01-20 12:56:38 +0530

    te

--- src/Components/Portfollio.jsx
+++ src/Components/Portfollio.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Portfolionav from '../portfolio/Portfolionav'
+import { Link } from 'react-router-dom'
 
 const Portfollio = () => {
   return (
     <>
    {/* <Portfolionav/> */}
     <div className="portfolio">
-        About me
+      <center>
+      About me
+      testyuiuu
+        
+        
+        
+
+        <nav>
+      <a href="#section1">Go to Section 1</a>
+      <a href="#section2">Go to Section 2</a>
+    </nav>
+    <div id="section1" style={{ height: '100vh', backgroundColor: 'lightblue' }}>
+      <h2>Section 1</h2>
+    </div>
+    <div id="section2" style={{ height: '100vh', backgroundColor: 'lightgreen' }}>
+      <h2>Section 2</h2>
+    </div>
+  
+
+
+
+
+        </center>
     </div>
     </>
   )
